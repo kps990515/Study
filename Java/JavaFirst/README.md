f5d8c1874e13592ce5f9bf4c54bda26f5526e3e1
unknown <kps990515@gmail.com>
2017-09-06 03:56:05 +0900

    test

---
+++ Java/JavaFirst/README.md
@@ -0,0 +1,17 @@
+# Fast Campus 안드로이드 스쿨
+
+## 1일차
+- 함수만들기
+> 접근제한자 + 리턴타입 + 함수명 +  (파라미터){...}
+- 접근제한자
+> public / private / protected / default / (static)
+- GIT 작동방법
+> workspace -> [add] -> stage -> [commit] -> git repository
+- 자바 파일의 컴파일 방식
+>* JIT : 실행 시 컴파일 / 끝나면 삭제 -> 느리지만 저용량
+>* AOT : 설치 시 컴파일 / 계속 유지 -> 빠르지만 고용량
+- 변수, 자료형, 상수 등 자바기초
+>* [Code예시](https://github.com/kps990515/Github/blob/master/src/Hello.java)
+>* [Github 참고](https://rogerdudler.github.io/git-guide/index.ko.html)
+>* [Github 자세히](https://nolboo.kim/blog/2013/10/06/github-for-beginner/)
+>* [Github 실습](http://learnbranch.urigit.com)
---
+++ Java/JavaFirst/src/Hello.java
@@ -0,0 +1,67 @@
+
+public class Hello {
+/**
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		// 정수형의 연산은 기본 int로 계산된다
+		// long형은 뒤에 L을 붙인다
+		byte a = 10;
+		byte b = 11;
+		
+		// byte로 하고 싶으면 강제형변환 한다
+		byte c = (byte)(a+b);
+		
+		// 실수형의 연산은 double형으로 계산된다
+		// float형은 뒤에 f를 붙인다
+		
+		float d = 3.14f;
+		float e = 4.1928f;
+		
+		// 실수 연산은 직접하지 않는다.
+		float f = Float.sum(d, e);
+		
+		double g = 5.232;
+		double h = 6.323131;
+		double i = Double.sum(g, h);
+		
+		int number = changeStringToInteger("123");
+		int result = number+123123;
+		test();
+		
+	}
+	/**
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public String changeNumberToString(int number)
+	{
+		//숫자를 스트링으로 바꿀때는 Integer.tostring을 써도 되지만 아래방법을 주로 사용한다
+		return number+"";
+	}
+	/**
+	 * 
+	 * @param word
+	 * @return
+	 */
+	public static int changeStringToInteger(String word)
+	{
+		//문자열을 숫자로 변환
+		return Integer.parseInt(word);
+	}
+	public static void test()
+	{
+		int a=2+3;
+		int b=2-3;
+		int c=2*3;
+		int d=2/3;
+		int e=5%3;
+		int f=10%3;
+		double dou=5.0%4.2;
+		System.out.println(" 2+3 : "+a+"\n 2-3 : "+b+"\n 2*3 : "+c+"\n 2/3 : "+d+"\n 5%3 : "+e+"\n 10%3 : "+f+"\n 5.0%4.2 : "+dou);
+	}
+}
+
